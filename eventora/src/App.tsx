f6879cf129a77f8ecf8b59eb7e4198fa9f089b52
Shabd <workwithshabd@gmail.com>
2026-08-10 14:01:49 +0530

    update

--- eventora/src/App.tsx
+++ eventora/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 
 import Home from "./pages/home";
 import EventDetail from "./pages/event";
