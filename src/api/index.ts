47d6bf50adfc415c863f88ddc3f37eb9aebd0167
Shabd <workwithshabd@gmail.com>
2026-08-16 12:19:47 +0530

    prepare backend deployment

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,6 @@
 export default function handler(req: any, res: any) {
-  console.log("VERCEL FUNCTION IS RUNNING");
-
   return res.status(200).json({
     success: true,
-    message: "Vercel function works",
-    method: req.method,
-    url: req.url,
+    message: "Vercel backend function is working",
   });
 }
